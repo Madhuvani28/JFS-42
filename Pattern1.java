2f0824494647a23edc91d2ca6500c6db01d4f0df
Madhuvani <158158329+Madhuvani28@users.noreply.github.com>
2025-09-09 13:47:57 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,15 @@
+package com.PatternExamples;
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++) {//6<=5
+			for(int j=1;j<=i;j++) {//5<=i
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
